cfeb6a08851bb2d974031ea0fbdcb87de362d763
Stanislav-Netreba <s.netreba.rs41@kpi.ua>
2024-09-17 15:03:31 +0300

    update

--- src/services/telegramService.js
+++ src/services/telegramService.js
@@ -65,9 +65,13 @@ class TelegramService {
 					await this.bot.sendMessage(chatId, 'An error occurred while sending the schedule.');
 				}
 			},
-			'/пасхалко':async (msg, chatId){
-				await this.bot.sendMessage(chatId, 'єбать цих [персон](https://uk.wikipedia.org/wiki/%D0%9D%D0%B0%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%96_%D0%BC%D0%B5%D0%BD%D1%88%D0%B8%D0%BD%D0%B8_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%96)', {reply_markup: "MarkdownV2"})
-			}
+			'/пасхалко': async (msg, chatId) => {
+				await this.bot.sendMessage(
+					chatId,
+					'єбать цих [персон](https://uk.wikipedia.org/wiki/%D0%9D%D0%B0%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%96_%D0%BC%D0%B5%D0%BD%D1%88%D0%B8%D0%BD%D0%B8_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%96)',
+					{ reply_markup: 'MarkdownV2' }
+				);
+			},
 		};
 	}
 
